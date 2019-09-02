059661e9e35420d53a685cc46b947a57ffb0fb06
squirmlabs <squirmlabs@gmail.com>
2019-09-01 23:57:01 -0700

    Importing a JSON text file to the CDR

--- toolkit-nodejs/importJSONFile.js
+++ toolkit-nodejs/importJSONFile.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import { read } from './file.js';
+const file = require('./file.js');
 
 // Helper function to import a JSON file.
 
 function importJsonFile(filePath) {
-  return read(filePath).then(textFileData => {
+  return file.read(filePath).then(textFileData => {
     return JSON.parse(textFileData);
   });
 }
 
-export default importJsonFile;
+module.exports = importJsonFile;
