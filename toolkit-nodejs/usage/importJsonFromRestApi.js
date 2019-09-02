5e66e10d3d44cad4f8f689a7bfdfd97d3bf4d64a
squirmlabs <squirmlabs@gmail.com>
2019-09-02 00:21:10 -0700

    Importing data from REST API usage

--- toolkit-nodejs/usage/importJsonFromRestApi.js
+++ toolkit-nodejs/usage/importJsonFromRestApi.js
@@ -1,27 +1,33 @@
 'use strict';
 
-// Require our "importJsonFromRestApi" toolkit function.
+// Modules
 const importJsonFromRestApi = require('../importJsonFromRestApi.js');
 
 const url =
   'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
-// Use our toolkit function to import data from the REST API.
+// USAGE: Use toolkit function to import data from the REST API.
 importJsonFromRestApi(url)
   .then(data => {
+    
     // Callback to handle imported data.
     const earthquakes = data.features.map(feature => {
       // Restructure incoming data to the CDR.
       const earthquake = Object.assign({}, feature.properties, {
         id: feature.id
       });
+
       return earthquake;
     });
+
     // Print the data to the console so that we can verify it.
     console.log(earthquakes);
+
   })
   .catch(err => {
+
     // Handle any error that might have occurred.
     console.error('An error occurred.');
     console.error(err.stack);
+
   });
