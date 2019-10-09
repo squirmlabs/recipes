b487ed74f786719690c17b6719b39f3bb32c28c3
squirmlabs <squirmlabs@gmail.com>
2019-10-09 15:50:43 -0700

    Add event loop, slice, search

--- event-loop/callstack.js
+++ event-loop/callstack.js
@@ -1,3 +1,14 @@
+// JavaScript programmers like to use words like, “event-loop”, “non-blocking”, “callback”, “asynchronous”, “single-threaded” and “concurrency”.
+
+// We say things like “don’t block the event loop”, “make sure your code runs at 60 frames-per-second”, “well of course, it won’t work, that function is an asynchronous callback!”
+
+// If you’re anything like me, you nod and agree, as if it’s all obvious, even though you don’t actually know what the words mean; and yet, finding good explanations of how JavaScript actually works isn’t all that easy, so let’s learn!
+
+// With some handy visualisations, and fun hacks, let’s get an intuitive understanding of what happens when JavaScript runs.
+
+// Transcript: http://2014.jsconf.eu/speakers/philip...
+
+
 function multiply(a, b) {
   return a * b;
 }
